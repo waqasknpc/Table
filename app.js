ee7770ff4f24141156c5b158746e5852a105545a
waqasknpc <89443569+waqasknpc@users.noreply.github.com>
2024-03-08 01:04:55 +0500

    Add files via upload
    Write Down HTML and Console

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ if(isNaN(table)){
     alert("Error: Please write only the number of which you want to write the table")
     reload();
 } else{
-    console.log("Multiplication table for " + table + " :");
+    console.log("Multiplication Table for " + table + " :");
+    document.write("<center><h2>")
+    document.write("Multiplication Table for " + table + " :");
+    document.write("</h2></center>")
 for (var i = 1; i <= 10; i++) {
     console.log( table + " x " + i + " = " + (table * i));
+    document.write("<center><b>")
+    document.write( table + " x " + i + " = " + (table * i));
+    document.write("</center></b>")
 }
+    document.write("<center><h5>")
+    document.write("Also Write Table of " + table + " in Console");
+    document.write("</h5></center>")
 }
