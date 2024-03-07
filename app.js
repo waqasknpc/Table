176de1a87ca47ff1fdf0be40e923b2e2226cb611
waqasknpc <89443569+waqasknpc@users.noreply.github.com>
2024-03-08 00:24:42 +0500

    Add files via upload
    Correct Table File Upload

--- app.js
+++ app.js
@@ -1,37 +1,10 @@
-alert("Simple Calculator")
-var firstValue = prompt("Please Enter First Number")
-if(isNaN(firstValue)){
-    alert("Error: Please Enter Only Numbers")
+var table = prompt("Which table do you want to write?")
+if(isNaN(table)){
+    alert("Error: Please write only the number of which you want to write the table")
     reload();
+} else{
+    console.log("Multiplication table for " + table + " :");
+for (var i = 1; i <= 10; i++) {
+    console.log( table + " x " + i + " = " + (table * i));
 }
-var operator = prompt("What do you want? + , - , * and /")
-var secondValue = prompt("Please Enter Second Number")
-if(isNaN(secondValue)){
-    alert("Error: Please Enter Only Numbers")
-    reload();
-}
-
-var firstValue = parseInt(firstValue)
-var secondValue = parseInt(secondValue)
-
-
-if(operator == "+"){
-    var result = firstValue + secondValue;
-    alert("Addition of two Numbers " + firstValue + " + " + secondValue  + " = " + result);
-    firstValue, secondValue, operator ="";
-} else if(operator == "-"){
-    var result = firstValue + secondValue;
-    alert("Subtraction of two Numbers " + firstValue + " - " + secondValue  + " = " + result);
-    firstValue, secondValue, operator ="";
-} else if(operator == "*"){
-    var result = firstValue * secondValue;
-    alert("Multiplication of two Numbers " + firstValue + " x " + secondValue  + " = " + result);
-    firstValue, secondValue, operator ="";
-} else if(operator == "/"){
-    var result = firstValue / secondValue;
-    alert("Division of two Numbers " + firstValue + " / " + secondValue  + " = " + result);
-    firstValue, secondValue, operator ="";
-} else {
-    alert("Error: Please Enter Correct Operator. e.g  + , - , * and /")
-    firstValue, secondValue, operator ="";
 }
